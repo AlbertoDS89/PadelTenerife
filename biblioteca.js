b57990ffb5cd6f9a787edc2e1f848beb6078f9ee
AlbertoDS89 <AlbertoDS89@gmail.com>
2017-12-29 19:38:38 +0000

    commit base datos palas

---
+++ biblioteca.js
@@ -0,0 +1,184 @@
+var palas = [
+
+    {
+      name: 'Bullpadel Black Dragon 2.0',
+      marca: 'Bullpadel',
+      precio: '350,00€',
+      precioDto: '330,00€',
+      tipo: 'equilibrada',
+      cantidad: 40,
+      enStock: true,
+      temporada: 2018,
+      vendidas: 20,
+      fotos: ['BullpadelBlackDragon1.jpg', 'BullpadelBlackDragon2.jpg', 'BullpadelBlackDragon3.jpg']
+    },
+
+    {
+      name: 'Bullpadel Hack 2018',
+      marca: 'Bullpadel',
+      precio: '330,00€',
+      precioDto: '296,95€',
+      tipo: 'potencia',
+      cantidad: 20,
+      enStock: true,
+      temporada: 2018,
+      vendidas: 44,
+      fotos: ['BullpadelHack20181.jpg', 'BullpadelHack20182.jpg', 'BullpadelHack20183.jpg']
+    },
+
+    {
+      name: 'Bullpadel Vertex Control 2017',
+      marca: 'Bullpadel',
+      precio: '283,00€',
+      precioDto: '169,00€',
+      tipo: 'control',
+      cantidad: 3,
+      enStock: false,
+      temporada: 2017,
+      vendidas: 167,
+      fotos: ['BullpadelVertexControl20171.jpg', 'BullpadelVertexControl20172.jpg']
+    },
+
+    {
+      name: 'Bullpadel K3 Pro 2016',
+      marca: 'Bullpadel',
+      precio: '167,00€',
+      precioDto: '79,95€',
+      tipo: 'control',
+      cantidad: 28,
+      enStock: true,
+      temporada: 2016,
+      vendidas: 58,
+      fotos: ['BullpadelK3Pro20161.jpg', 'BullpadelK3Pro20162.jpg']
+    },
+
+    {
+      name: 'Dunlop Hot Shot Extreme',
+      marca: 'Dunlop',
+      precio: '300,00€',
+      precioDto: '270,00€',
+      tipo: 'equilibrada',
+      cantidad: 48,
+      enStock: true,
+      temporada: 2017,
+      vendidas: 27,
+      fotos: ['DunlopHotShotExtreme.jpg']
+    },
+
+    {
+      name: 'Dunlop Hot Shot Elite',
+      marca: 'Dunlop',
+      precio: '300,00€',
+      precioDto: '280,00€',
+      tipo: 'control',
+      cantidad: 28,
+      enStock: true,
+      temporada: 2017,
+      vendidas: 45,
+      fotos: ['DunlopHotShotElite1.jpg', 'DunlopHotShotElite2.jpg', 'DunlopHotShotElite3.jpg']
+    },
+
+    {
+      name: 'Dunlop Luxury',
+      marca: 'Dunlop',
+      precio: '180,00€',
+      precioDto: '79,95€',
+      tipo: 'equilibrada',
+      cantidad: 18,
+      enStock: true,
+      temporada: 2014,
+      vendidas: 129,
+      fotos: ['DunlopLuxury1.jpg', 'DunlopLuxury2.jpg']
+    },
+
+    {
+      name: 'Dunlop Fusion 1.4',
+      marca: 'Dunlop',
+      precio: '250,00€',
+      precioDto: '69,95€',
+      tipo: 'control',
+      cantidad: 0,
+      enStock: false,
+      temporada: 2014,
+      vendidas: 145,
+      fotos: ['DunlopFusion1.jpg', 'DunlopFusion2.jpg', 'DunlopFusion3.jpg']
+    },
+
+    {
+      name: 'Dunlop Nitro',
+      marca: 'Dunlop',
+      precio: '250,00€',
+      precioDto: '59,95€',
+      tipo: 'control',
+      cantidad: 67,
+      enStock: true,
+      temporada: 2017,
+      vendidas: 45,
+      fotos: ['DunlopNitro1.jpg', 'DunlopNitro2.jpg', 'DunlopNitro3.jpg']
+    },
+
+    {
+      name: 'Head Graphene Touch Delta Hybrid',
+      marca: 'Head',
+      precio: '290,00€',
+      precioDto: '231,95€',
+      tipo: 'equilibrada',
+      cantidad: 37,
+      enStock: true,
+      temporada: 2017,
+      vendidas: 57,
+      fotos: ['HeadGrapheneTouchDeltaHybrid1.jpg', 'HeadGrapheneTouchDeltaHybrid2.jpg']
+    },
+
+    {
+      name: 'Head Graphene XT Delta Pro',
+      marca: 'Head',
+      precio: '269,95€',
+      precioDto: '121,95€',
+      tipo: 'equilibrada',
+      cantidad: 27,
+      enStock: true,
+      temporada: 2016,
+      vendidas: 97,
+      fotos: ['HeadGrapheneXTDeltaPro1.jpg', 'HeadGrapheneXTDeltaPro2.jpg']
+    },
+
+    {
+      name: 'Head Graphene Tornado E+',
+      marca: 'Head',
+      precio: '189,95€',
+      precioDto: '88,95€',
+      tipo: 'control',
+      cantidad: 2,
+      enStock: false,
+      temporada: 2016,
+      vendidas: 147,
+      fotos: ['HeadGrapheneTornadoE+1.jpg', 'HeadGrapheneTornadoE+2.jpg']
+    },
+
+    {
+      name: 'Adidas Adipower Carbon Control',
+      marca: 'Adidas',
+      precio: '365,00€',
+      precioDto: '209,95€',
+      tipo: 'control',
+      cantidad: 32,
+      enStock: true,
+      temporada: 2016,
+      vendidas: 22,
+      fotos: ['AdidasAdipowerCarbonControl1.jpg', 'AdidasAdipowerCarbonControl2.jpg', 'AdidasAdipowerCarbonControl3.jpg']
+    },
+
+    {
+      name: 'Adidas R5',
+      marca: 'Adidas',
+      precio: '85,00€',
+      precioDto: '49,90€',
+      tipo: 'equilibrada',
+      cantidad: 12,
+      enStock: true,
+      temporada: 2016,
+      vendidas: 78,
+      fotos: ['AdidasR51.jpg', 'AdidasR52.jpg']
+    },
+];
