578f4552b3dadf2a037554b239bb67a12238f229
AlbertoDS89 <AlbertoDS89@gmail.com>
2017-12-30 21:41:07 +0000

    commit pelotas y zapatillas

--- biblioteca.js
+++ biblioteca.js
@@ -180,5 +180,225 @@ var palas = [
       temporada: 2016,
       vendidas: 78,
       fotos: ['AdidasR51.jpg', 'AdidasR52.jpg']
-    },
+    }
 ];
+
+var pelotas = [
+
+    {
+      name: 'Pelotas Head Padel Pro',
+      marca: 'Head',
+      precio: '6,00€',
+      precioDto: '4,90€',
+      cantidad: 80,
+      enStock: true,
+      vendidas: 620,
+      fotos: ['PelotasHeadPadelPro.jpg']
+    },
+
+    {
+      name: 'Bullpadel Premium Pro',
+      marca: 'Bullpadel',
+      precio: '4,50€',
+      precioDto: '3,95€',
+      cantidad: 50,
+      enStock: true,
+      vendidas: 416,
+      fotos: ['BullpadelPremiumPro.jpg']
+    },
+
+    {
+      name: 'Bolas Nox Stinger',
+      marca: 'Nox',
+      precio: '6,95€',
+      precioDto: '4,95€',
+      cantidad: 97,
+      enStock: true,
+      vendidas: 578,
+      fotos: ['BolasNoxStinger.jpg']
+    },
+
+    {
+      name: 'Wilson Pro Padel',
+      marca: 'Wilson',
+      precio: '4,95€',
+      precioDto: '3,40€',
+      cantidad: 197,
+      enStock: true,
+      vendidas: 378,
+      fotos: ['WilsonProPadel.jpg']
+    }
+];
+
+
+var zapatillas = [
+
+    {
+      name: 'Bullpadel Burton Azul Celeste',
+      marca: 'Bullpadel',
+      precio: '52,00€',
+      precioDto: '31,20€',
+      temporada: 2017,
+      cantidad: 30,
+      enStock: true,
+      vendidas: 70,
+      fotos: ['BullpadelBurtonAzulCeleste1.jpg', 'BullpadelBurtonAzulCeleste2.jpg', 'BullpadelBurtonAzulCeleste3.jpg']
+    },
+
+    {
+      name: 'Bullpadel Burton Gris Oscuro',
+      marca: 'Bullpadel',
+      precio: '52,00€',
+      precioDto: '31,20€',
+      temporada: 2017,
+      cantidad: 33,
+      enStock: true,
+      vendidas: 30,
+      fotos: ['BullpadelBurtonGrisOscuro1.jpg', 'BullpadelBurtonGrisOscuro2.jpg']
+    },
+
+    {
+      name: 'Bullpadel Benka Amarillo Negro 2014',
+      marca: 'Bullpadel',
+      precio: '89,00€',
+      precioDto: '19,95€',
+      temporada: 2014,
+      cantidad: 0,
+      enStock: false,
+      vendidas: 112,
+      fotos: ['BullpadelBenkaAmarilloNegro2014.jpg']
+    },
+
+    {
+      name: 'Bullpadel Bale Azul Real',
+      marca: 'Bullpadel',
+      precio: '52,00€',
+      precioDto: '22,95€',
+      temporada: 2016,
+      cantidad: 30,
+      enStock: true,
+      vendidas: 56,
+      fotos: ['BullpadelBaleAzulReal1.jpg', 'BullpadelBaleAzulReal2.jpg']
+    },
+
+    {
+      name: 'Head Revolt Pro 2.0 Clay Negro Rojo',
+      marca: 'Head',
+      precio: '139,95€',
+      precioDto: '83,90€',
+      temporada: 2017,
+      cantidad: 28,
+      enStock: true,
+      vendidas: 73,
+      fotos: ['HeadRevoltPro2.0ClayNegroRojo1.jpg', 'HeadRevoltPro2.0ClayNegroRojo2.jpg', 'HeadRevoltPro2.0ClayNegroRojo3.jpg']
+    },
+
+    {
+      name: 'Head Revolt Pro 2.0 Ltd Negro',
+      marca: 'Head',
+      precio: '145,00€',
+      precioDto: '84,00€',
+      temporada: 2017,
+      cantidad: 78,
+      enStock: true,
+      vendidas: 23,
+      fotos: ['HeadRevoltPro2.0LtdNegro1.jpg', 'HeadRevoltPro2.0LtdNegro2.jpg', 'HeadRevoltPro2.0LtdNegro3.jpg', 'HeadRevoltPro2.0LtdNegro4.jpg']
+    },
+
+    {
+      name: 'Head Lazer Negro Naranja',
+      marca: 'Head',
+      precio: '64,95€',
+      precioDto: '21,95€',
+      temporada: 2015,
+      cantidad: 28,
+      enStock: true,
+      vendidas: 56,
+      fotos: ['HeadLazerNegroNaranja1.jpg', 'HeadLazerNegroNaranja2.jpg']
+    },
+
+    {
+      name: 'Head Revolt Pro Padel Rojo',
+      marca: 'Head',
+      precio: '124,95€',
+      precioDto: '49,95€',
+      temporada: 2015,
+      cantidad: 65,
+      enStock: true,
+      vendidas: 72,
+      fotos: ['HeadRevoltProPadelRojo1.jpg']
+    },
+
+    {
+      name: 'Adidas Novak Pro',
+      marca: 'Adidas',
+      precio: '139,95€',
+      precioDto: '109,95€',
+      temporada: 2017,
+      cantidad: 35,
+      enStock: true,
+      vendidas: 42,
+      fotos: ['AdidasNovakPro1.jpg', 'AdidasNovakPro2.jpg', 'AdidasNovakPro3.jpg']
+    },
+
+    {
+      name: 'Adidas Barricade 2017',
+      marca: 'Adidas',
+      precio: '139,95€',
+      precioDto: '94,95€',
+      temporada: 2017,
+      cantidad: 75,
+      enStock: true,
+      vendidas: 29,
+      fotos: ['AdidasBarricade20171.jpg', 'AdidasBarricade20172.jpg', 'AdidasBarricade20173.jpg']
+    },
+
+    {
+      name: 'Adidas Adizero Club Negro',
+      marca: 'Adidas',
+      precio: '79,95€',
+      precioDto: '59,95€',
+      temporada: 2017,
+      cantidad: 35,
+      enStock: true,
+      vendidas: 29,
+      fotos: ['AdidasAdizeroClubNegro1.jpg', 'AdidasAdizeroClubNegro2.jpg', 'AdidasAdizeroClubNegro3.jpg']
+    },
+
+    {
+      name: 'Nike Zoom Vapor 9.5 Tour',
+      marca: 'Nike',
+      precio: '130,00€',
+      precioDto: '117,00€',
+      temporada: 2017,
+      cantidad: 33,
+      enStock: true,
+      vendidas: 29,
+      fotos: ['NikeZoomVapor9.5Tour1.jpg', 'NikeZoomVapor9.5Tour2.jpg']
+    },
+
+    {
+      name: 'Nike Zoom Ultra Cly',
+      marca: 'Nike',
+      precio: '100,00€',
+      precioDto: '89,95€',
+      temporada: 2017,
+      cantidad: 73,
+      enStock: true,
+      vendidas: 19,
+      fotos: ['NikeZoomUltraCly1.jpg', 'NikeZoomUltraCly2.jpg']
+    },
+
+    {
+      name: 'Nike Court Lite Cly',
+      marca: 'Nike',
+      precio: '55,00€',
+      precioDto: '49,95€',
+      temporada: 2016,
+      cantidad: 13,
+      enStock: true,
+      vendidas: 147,
+      fotos: ['NikeCourtLiteCly1.jpg', 'NikeCourtLiteCly2.jpg', 'NikeCourtLiteCly3.jpg']
+    },
+
+  ];
