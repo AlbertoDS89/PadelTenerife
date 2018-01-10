e5e21552e801f6a45f252e02a28ed5ca446ce26c
Alberto Dominguez <AlbertoDS89@gmail.com>
2018-01-10 18:56:54 +0000

    arreglado logo nike

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-Js Padel
+var visualizacionProducto = document.getElementById('visualizacionProducto');
+var producto = productos[1];
+var numeroImagenes=producto.fotos.length;
+function addImagen(contenedor, urlImagen){
+  var imagen =  document.createElement('img');
+  contenedor.appendChild(imagen);
+    imagen.src = 'imagenes/' + urlImagen;
+}
+
+var contador;
+for (contador=0; contador<numeroImagenes; contador ++){
+  addImagen(visualizacionProducto, producto.fotos[contador]);
+}
