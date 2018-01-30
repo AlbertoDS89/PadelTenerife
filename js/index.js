341217174a1be015c5e78b1b57ff4dac0d846c89
Alberto Dominguez <AlbertoDS89@gmail.com>
2018-01-30 18:09:08 +0000

    javascript mejorado

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
+function recopilarFotosProducto(elemento){
+  var datos = [];
+  for (i = 0; i < elemento.fotos.length; i++) {
+    var obj = {};
+    obj.href = './imagenes/' + elemento.fotos[i];
+    obj.title = elemento.name;
+    datos.push(obj);
+  }
+  return datos;
+}
 
-$(document).ready(function () {
-    AOS.init();
-});
-
-$(document).ready(function() {
+function inicializarCookieBar(){
   $.cookieBar({
     message: 'Esta web utiliza cookies. ¿Acepta las condiciones de uso?.',
     acceptButton: true,
@@ -18,19 +24,19 @@ $(document).ready(function() {
     policyText: 'Leer más',
     policyURL: './paginasadicionales/Pcookies.html'
   });
-});
+}
 
 function mostrarProducto(elemento) {
     $('.nombreProduc > h1').html(elemento.name);
-    $('.precio > p').html('<strong>Precio:</strong> ' + elemento.precio);
-    $('.preciodesc > p').html('<strong>Precio final:</strong> ' + elemento.precioDto);
-    $('.marca > p').html('<strong>Marca:</strong> ' + elemento.marca);
-    $('.year > p').html('<strong>Año:</strong>'  + elemento.temporada);
-    $('.tipo > p').html('<strong>Tipo:</strong> ' + elemento.tipo);
-    $('.unidRest > p').html('<strong>Unidades Restantes:</strong> ' + elemento.cantidad);
-    $('.unidVend > p').html('<strong>Unidades Vendidas:</strong> ' + elemento.vendidas);
+    $('.precio-value').html(elemento.precio);
+    $('.preciodesc-value').html(elemento.precioDto);
+    $('.marca-value').html(elemento.marca);
+    $('.year-value').html(elemento.temporada);
+    $('.tipo-value').html(elemento.tipo);
+    $('.unidRest-value').html(elemento.cantidad);
+    $('.unidVend-value').html(elemento.vendidas);
 
-    if (elemento.enStock === true) {
+    if (elemento.enStock) {
       $('.stock > p').html('<a title="comprar" href="./paginasadicionales/comprar.html"><img src="./imagenes/comprar.png" alt="comprar" />');
     } else {
       $('.stock > p').html('<strong>En stock:</strong> No');
@@ -39,65 +45,54 @@ function mostrarProducto(elemento) {
     $('.desc > p').html(elemento.descripcion);
 
     // Arreglar (con elementos y no como cadena de texto)
-    $('#foto-producto').html("<a id=\"enlace-producto\" href=\"#\" class=\"swipebox\"><img id=\"imagen-producto\"></a>");
+    $('#foto-producto').html('<a id="enlace-producto" href="#" class="swipebox"><img id="imagen-producto\"></a>');
 
     $('#imagen-producto').attr('src', './imagenes/' + elemento.fotos[0]);
 
-    $( '#enlace-producto' ).click( function( e ) {
+    $('#enlace-producto').click(function(e) {
       e.preventDefault();
-
-      var datos = [];
-      for (i = 0; i < elemento.fotos.length; i++) {
-        var obj = {};
-        obj.href = './imagenes/' + elemento.fotos[i];
-        obj.title = elemento.name;
-        datos.push(obj);
-      }
-
-      console.log('muestra:', datos);
-      $.swipebox( datos );
-    } );
+      var fotosDelProducto = recopilarFotosProducto(elemento);
+      $.swipebox(fotosDelProducto);
+    });
 }
 
-var options = {
-    url: "./js/productos.json",
-    getValue: "name",
-    cssClasses: "item-name",
-    template: {
-        type: "iconLeft",
-        fields: {
-            iconSrc: "icon"
-        }
-    },
+function inicializarBuscador(){
+  var options = {
+      url: "./js/productos.json",
+      getValue: "name",
+      cssClasses: "item-name",
+      template: {
+          type: "iconLeft",
+          fields: {
+              iconSrc: "icon"
+          }
+      },
 
-    list: {
-        onClickEvent: function () {
-            var elemento = $("#buscador").getSelectedItemData();
-            mostrarProducto(elemento);
-        },
-        maxNumberOfElements: 10,
-        match: {
-            enabled: true
-        },
-        showAnimation: {
-            type: "slide"
-        },
-        hideAnimation: {
-            type: "slide"
-        }
+      list: {
+          onClickEvent: function () {
+              var elemento = $("#buscador").getSelectedItemData();
+              mostrarProducto(elemento);
+          },
+          maxNumberOfElements: 10,
+          match: {
+              enabled: true
+          },
+          showAnimation: {
+              type: "slide"
+          },
+          hideAnimation: {
+              type: "slide"
+          }
 
-    },
-    theme: "round"
+      },
+      theme: "round"
 
-};
+  };
+  $("#buscador").easyAutocomplete(options);
+}
 
-$("#buscador").easyAutocomplete(options);
-
-$("#enviarform").click(function(e) {
-    var correcto = ($(".marcas-padel input[type=checkbox]:checked").length > 0);
-
-    if (correcto == false) {
-      alert('Selecciona al menos una de las marcas');
-      return false;
-    }
+$(document).ready(function() {
+  AOS.init();
+  inicializarCookieBar();
+  inicializarBuscador();
 });
